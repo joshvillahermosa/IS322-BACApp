5989a08e78a5d684ba93a636cbf83207feb47cd6
joshvillahermosa <joshvillahermosa@gmail.com>
2014-04-24 14:34:26 -0500

    Fixed view

--- js/views/BAC.js
+++ js/views/BAC.js
@@ -1,13 +1,16 @@
-var BAC = new Backbone.View.extend({
+var BAC =  Backbone.View.extend({
 	el: '#load',
 
 	initialize: function(){
-		this.calc= 'Test';
-		//this.render();
+		this.calc= '<div class="row" style="text-align: center"><div class="col-sm-12"><button class="btn btn-default" style="display: block;width: 100%">Beer</button></div><div class="col-sm-12"><button class="btn btn-warning" style="display: block;width: 100%">Warning</button></div><div class="col-sm-12"><button class="btn btn-info" style="display: block;width: 100%">Wine</button></div><div class="col-sm-12"><button class="btn btn-default btn-lg" style="display: block;width: 100%">%BAC%</button><buton class="btn btn-danger">Calculate</button></div></div>';
 	},
 
 	render: function(){
 		this.$el.html(this.calc);
+	},
+
+	calcBac: function(){
+		//Alex
 	}
 
 });
