ba5d36cfebd743f756e2af9ce0fbc8d8458e6d6e
joshvillahermosa <joshvillahermosa@gmail.com>
2014-04-24 18:52:27 -0500

    Added initialize, render, and createMap function

--- js/views/Area.js
+++ js/views/Area.js
@@ -16,7 +16,8 @@ var Area = Backbone.View.extend({
 			center: new google.maps.LatLng(40.7430473, -74.1777488),
 			zoom: 8
 		}
-		var map = new google.maps($('"#'+this.canvasId+'"'))
+		var map = new google.maps($('"#'+this.canvasId+'"'), mapOptions);
+		google.maps.events.addDomListerner(window, 'load', initialize);
 	}
 
 });
