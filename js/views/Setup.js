1de7762a0e7697b886eb545cb1fbc86ba0090f06
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-05 13:58:38 -0500

    Changed form andadded method of inputing data to local storage

--- js/views/Setup.js
+++ js/views/Setup.js
@@ -7,7 +7,7 @@ var Setup =  Backbone.View.extend({
 	},
 
 	initialize: function(){
-		this.form = '<form role="form"><div class="form-group"><label for="name">Name</label><input id="name" name="name" type="text" class="form-control" placeholder="Name" required></div><div class="form-group"><label for="email">Email</label><input id="email" name="email" type="text" class="form-control" placeholder="Email" required></div><div class="from-group"><label for="age">Age</label><input id=""age type="date" name="age" class="form-control" required ></div><div class="from-group"><label for="weight">Weight</label><input id="weight" type="number" min="50" max="500" name="weight" class="form-control" required></div><div class="radio-inline"><div class="row"><div class="col-xs-6"><label><input type="radio" name="sex" value="0.73">Male</label></div><div class="col-xs-6"><label><input type="radio" name="sex" value="0.66">Female</label></div></div></div><br><button type="button" class="btn btn-success" id="create">Set/Update</button></form>';
+		this.form = '<form role="form"><div class="form-group"><label for="name">Name</label><input id="name" name="name" type="text" class="form-control" placeholder="Name" required></div><div class="form-group"><label for="email">Email</label><input id="email" name="email" type="text" class="form-control" placeholder="Email" required></div><div class="from-group"><label for="birthday">Birthday</label><input id="birthday" type="date" name="birthday" class="form-control" required ></div><div class="from-group"><label for="weight">Weight</label><input id="weight" type="number" min="50" max="500" name="weight" class="form-control" required></div><div class="radio-inline"><div class="row"><div class="col-xs-6"><label><input type="radio" name="sex" value="0.73">Male</label></div><div class="col-xs-6"><label><input type="radio" name="sex" value="0.66">Female</label></div></div></div><br><button type="button" class="btn btn-success" id="create">Set</button></form>';
 		//this.render();
 	},
 
@@ -24,7 +24,7 @@ var Setup =  Backbone.View.extend({
 	createProfile: function(){
 		var name = $('#name').val();
 		var email = $('#email').val();
-		var age = $('#age').val();
+		var birthday = $('#birthday').val();
 		var weight = $('#weight').val();
 		var genderBac = $('input[name="sex"]:checked').val();
 		var personSetup = new Person({
@@ -32,7 +32,7 @@ var Setup =  Backbone.View.extend({
 			"name": name,
 			"email": email,
 			"genderBac": genderBac,
-			"age": age,
+			"birthday": birthday,
 			"weight": weight
 		});
 
