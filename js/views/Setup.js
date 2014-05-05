13ac9c63124ed3024e48fd229a8235d0da82a88b
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-05 11:40:13 -0500

    Added createProfile function to create new user profile

--- js/views/Setup.js
+++ js/views/Setup.js
@@ -2,16 +2,17 @@ var Setup =  Backbone.View.extend({
 	el: '#load',
 
 	events:{
-		//"click #update": "updateYou"
-		"onchange": "createProfile"
+		"click #create": "createProfile"
+		//"onchange": "createProfile"
 	},
 
 	initialize: function(){
-		this.form = '<form role="form"><div class="form-group"><label for="name">Name</label><input id="name" name="name" type="text" class="form-control" placeholder="Name" required></div><div class="form-group"><label for="email">Email</label><input name="email" type="text" class="form-control" placeholder="Email" required></div><div class="from-group"><label for="age">Age</label><input type="date" name="age" class="form-control" required></div><div class="from-group"><label for="weight">Weight</label><input type="number" min="50" max="500" name="weight" class="form-control" required></div><div class="radio-inline"><div class="row"><div class="col-xs-6"><label><input type="radio" name="sex" value="male">Male</label></div><div class="col-xs-6"><label><input type="radio" name="sex" value="female">Female</label></div></div></div><br><button type="button" class="btn btn-success" id="update">Set/Update</button></form>';
+		this.form = '<form role="form"><div class="form-group"><label for="name">Name</label><input id="name" name="name" type="text" class="form-control" placeholder="Name" required></div><div class="form-group"><label for="email">Email</label><input id="email" name="email" type="text" class="form-control" placeholder="Email" required></div><div class="from-group"><label for="age">Age</label><input id=""age type="date" name="age" class="form-control" required ></div><div class="from-group"><label for="weight">Weight</label><input id="weight" type="number" min="50" max="500" name="weight" class="form-control" required></div><div class="radio-inline"><div class="row"><div class="col-xs-6"><label><input type="radio" name="sex" value="0.73">Male</label></div><div class="col-xs-6"><label><input type="radio" name="sex" value="0.66">Female</label></div></div></div><br><button type="button" class="btn btn-success" id="create">Set/Update</button></form>';
 		//this.render();
 	},
 
 	render: function(){
+		this.newSave = new People();
 		this.$el.html(this.form);
 	},
 
@@ -21,8 +22,21 @@ var Setup =  Backbone.View.extend({
 	}*/
 
 	createProfile: function(){
-		var a = $('#name').val();
-		console.log("Updated name: "+a);
-		$('#name').setAttr('value', a);
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var age = $('#age').val();
+		var weight = $('#weight').val();
+		var genderBac = $('input[name="sex"]:checked').val();
+		var personSetup = new Person({
+			"firstTime": false,
+			"name": name,
+			"email": email,
+			"genderBac": genderBac,
+			"age": age,
+			"weight": weight
+		});
+
+		this.newSave.add(personSetup);
+		personSetup.save();
 	}
 })
