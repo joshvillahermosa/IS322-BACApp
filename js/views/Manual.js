f4fc8ad8c8e148840683c0066960cf818f11718b
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-06 17:43:17 -0500

    Emptys well when new BAC is calculated

--- js/views/Manual.js
+++ js/views/Manual.js
@@ -8,7 +8,7 @@ var Manual =  Backbone.View.extend({
 	initialize: function(){
 		this.user = new You();
 		this.you = this.user.retrieveProfile();
-		this.form = '<form role="form"><div class="form-group"><label for="acv">Alcohol Content Volume</label><input id="acv" name="acv" class="form-control" placeholder="0" required></div><div class="form-group"><label for="oz">Ounzes</label><input id="oz" name="oz" class="form-control" placeholder="0" required></div><div class="from-group"><label for="time">Time</label><input id="time" type="number" class="form-control" required></div><br><button id="manualBac" class="btn btn-success">Calculate</button></form><div class="well" id="BAC"></div>';
+		this.form = '<form role="form"><div class="form-group"><label for="acv">Alcohol Content Volume</label><input id="acv" name="acv" class="form-control" placeholder="0" required></div><div class="form-group"><label for="oz">Ounzes</label><input id="oz" name="oz" class="form-control" placeholder="0" required></div><div class="from-group"><label for="time">Time</label><input id="time" type="number" min="0" max="24" class="form-control" required></div><br><button id="manualBac" class="btn btn-success">Calculate</button></form><div class="well" id="BAC"><span > Your BAC Level is</span><span id="BACLevel"></span></div>';
 
 		this.manualBac = new BAC();
 		//this.render();
@@ -23,6 +23,7 @@ var Manual =  Backbone.View.extend({
 		var oz = $('#oz').val();
 		var time = $('#time').val();
 		var bac = this.manualBac.calcBac(acv, oz, this.you.weight, this.you.genderBac, time);
-		$('#BAC').append('<p>'+bac+'</p>');
+		$('#BACLevel').empty();
+		$('#BACLevel').append('<p>'+bac+'</p>');
 	}
 })
