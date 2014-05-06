50b1b14205760e2dedb9b1d7d2b3283a7490d53a
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-06 14:26:12 -0500

    Added User preferecnes in defaults

--- js/models/Person.js
+++ js/models/Person.js
@@ -7,5 +7,14 @@ var Person = Backbone.Model.extend({
 		"birthday": "",
 		"weight": 100,
 		"BACLevels": []
+		"beer1": [],
+		"beer2": [],
+		"beer3": [],
+		"wine1": [],
+		"wine2": [],
+		"wine3": [],
+		"hl1": [],
+		"hl2": [],
+		"hl3": []
 	}
 })
