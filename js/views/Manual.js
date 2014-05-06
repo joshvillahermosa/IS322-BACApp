dbd447ab5af3730a4e5d665c22465a7c5213101b
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-06 11:52:27 -0500

    Linked new formula

--- js/views/Manual.js
+++ js/views/Manual.js
@@ -8,7 +8,7 @@ var Manual =  Backbone.View.extend({
 	initialize: function(){
 		this.user = new You();
 		this.you = this.user.retrieveProfile();
-		this.form = '<form role="form"><div class="form-group"><label for="acv">Alcohol Content Volume</label><input id="acv" name="acv" type="" class="form-control" placeholder="0" required></div><div class="from-group"><label for="time">Time</label><input id="time" type="number" class="form-control" required></div><br><button id="manualBac" type="" class="btn btn-success">Calculate</button></form><div class="well" id="BAC"></div>';
+		this.form = '<form role="form"><div class="form-group"><label for="acv">Alcohol Content Volume</label><input id="acv" name="acv" class="form-control" placeholder="0" required></div><div class="form-group"><label for="oz">Ounzes</label><input id="oz" name="oz" class="form-control" placeholder="0" required></div><div class="from-group"><label for="time">Time</label><input id="time" type="number" class="form-control" required></div><br><button id="manualBac" class="btn btn-success">Calculate</button></form><div class="well" id="BAC"></div>';
 
 		this.manualBac = new BAC();
 		//this.render();
@@ -20,8 +20,9 @@ var Manual =  Backbone.View.extend({
 
 	calcBac: function(){
 		var acv = $('#acv').val();
+		var oz = $('#oz').val();
 		var time = $('#time').val();
-		var bac = this.manualBac.calcBac(acv, this.you.weight, this.you.genderBac, time);
+		var bac = this.manualBac.calcBac(acv, oz, this.you.weight, this.you.genderBac, time);
 		$('#BAC').append('<p>'+bac+'</p>');
 	}
 })
