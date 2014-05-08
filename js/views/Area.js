b4cbbe3a5a25c2137178486b109c01fcf10a7a59
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-07 20:58:54 -0500

    Fixed location settings

--- js/views/Area.js
+++ js/views/Area.js
@@ -9,8 +9,8 @@ var Area = Backbone.View.extend({
 
 	render: function(){
 		this.$el.html(this.area);
-		this.getLocation();
-		console.log(this.lat+' '+this.lng+' - Confirmed');
+		var curLocation = this.getLocation();
+		console.log(curLocation.lat+' '+curLocation.lng+' - Confirmed');
 		this.createMap(this.lat, this.lng);
 		//google.maps.event.addDomListener(window, 'load', this.createMap);
 		
@@ -21,38 +21,45 @@ var Area = Backbone.View.extend({
 		//var mlong = lng;
 		//alert(lat+' '+lng);
 		var mapOptions = {
-			center: new google.maps.LatLng(40.7430473, -74.1777488),
-			//center: new google.maps.LatLng(lat, lng),
+			//center: new google.maps.LatLng(40.7430473, -74.1777488),
+			center: new google.maps.LatLng(lat, lng),
 			zoom: 15
 		}
 		var map = new google.maps.Map(document.getElementById(this.canvasId), mapOptions);
 	},
 
 	getLocation: function(){
-		var self = this;
-		console.log('Test - getGeolocation');
 		if(navigator.geolocation){
-			//navigator.geolocation.getCurrentPosition(this.setLatLng);
-			navigator.geolocation.getCurrentPosition(function(position){
-				this.lat = position.coords.latitude;
-				this.lng = position.coords.longitude;
-			});
+			var location = navigator.geolocation.getCurrentPosition(this.setLatLng);
+			/*navigator.geolocation.getCurrentPosition(function(position){
+				var location = {"lat": 0, "lng":0};
+				location.lat = position.coords.latitude;
+				location.lng = position.coords.longitude;
+				return location;
+			});*/
 		}else{
 			alert('Either you have no internet connection or your browser is not supported');
+			console.log('Either you have no internet connection or your browser is not supported');
+			var location = null;
 		}
+		return location;
 	},
 
 	setLatLng: function(position){
-		console.log('Test - setLatLng');
-		return this.lat = position.coords.latitude;
-		console.log('Test - setLatLng-lat');
-		return this.lng = position.coords.longitude;
-		console.log('Test - setLatLng-lng');
-		console.log(this.lat+' '+this.lng);
+		var location = {lat: 0, lng: 0};
+		console.log('setLatLng() called');
+		location.lat = position.coords.latitude;
+		console.log('Lat recieved');
+		location.lng = position.coords.longitude;
+		console.log('Lng recieved');
+		console.log(location.lat+' '+location.lng);
+		return location;
 	},
 
 	throwErr: function(){
 		alert('Cannot get coords');
 	}
 
-});
+})
+
+var area = new Area()
