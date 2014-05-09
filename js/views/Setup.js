390d4d46ca3921fe212bad30b78e8ab9c3c6b330
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-08 20:24:04 -0500

    Added method to load bar(dafault drinks) when going though set up

--- js/views/Setup.js
+++ js/views/Setup.js
@@ -8,6 +8,8 @@ var Setup =  Backbone.View.extend({
 
 	initialize: function(){
 		this.form = '<form role="form"><div class="form-group"><label for="name">Name</label><input id="name" name="name" type="text" class="form-control" placeholder="Name" required></div><div class="form-group"><label for="email">Email</label><input id="email" name="email" type="text" class="form-control" placeholder="Email" required></div><div class="from-group"><label for="birthday">Birthday</label><input id="birthday" type="date" name="birthday" class="form-control" required ></div><div class="from-group"><label for="weight">Weight</label><input id="weight" type="number" min="50" max="500" name="weight" class="form-control" required></div><div class="radio-inline"><div class="row"><div class="col-xs-6"><label><input type="radio" name="sex" value="0.73">Male</label></div><div class="col-xs-6"><label><input type="radio" name="sex" value="0.66">Female</label></div></div></div><br><button type="button" class="btn btn-success" id="create">Set</button></form>';
+
+		this.loadBar();
 		//this.render();
 	},
 
@@ -40,5 +42,56 @@ var Setup =  Backbone.View.extend({
 		personSetup.save();
 
 		window.location.href = "#/BAC";
+	},
+
+	loadBar: function(){
+		var newBar = new Bar();
+		var alcohol = newBar.defaultAlcohol();
+
+		var alcoholBeerLength = alcohol.beer.length;
+		var alcoholWineLength = alcohol.wine.length;
+		var alcoholHlLength = alcohol.hl.length;
+
+		console.log('Loading Beer');
+
+		for(var i = 0; i < alcoholBeerLength; i++){
+			var beer = new Alcohol({
+				"name": alcohol.beer[i].name,
+				"alcoholContent": alcohol.beer[i].alcoholContent,
+				"ounces": alcohol.beer[i].ounces,
+				"timeConsumption":alcohol.beer[i].timeConsumption
+			});
+			newBar.add(beer);
+			beer.save()	
+		}
+
+		console.log('Finished Loading Beer');
+		console.log('Loading Wine');
+
+		for(var i = 0; i < alcoholWineLength; i++){
+			var wine = new Alcohol({
+				"name": alcohol.wine[i].name,
+				"alcoholContent": alcohol.wine[i].alcoholContent,
+				"ounces": alcohol.wine[i].ounces,
+				"timeConsumption":alcohol.wine[i].timeConsumption
+			});
+			newBar.add(wine);
+			wine.save()	
+		}
+
+		console.log('Finished Loading Wine');
+		console.log('Loading Hard Liquer');
+
+		for(var i = 0; i < alcoholHlLength; i++){
+			var hl = new Alcohol({
+				"name": alcohol.hl[i].name,
+				"alcoholContent": alcohol.hl[i].alcoholContent,
+				"ounces": alcohol.hl[i].ounces,
+				"timeConsumption":alcohol.hl[i].timeConsumption
+			});
+			newBar.add(hl);
+			hl.save()	
+		}
+		console.log('Finished Loading Hard Liquer');
 	}
 })
