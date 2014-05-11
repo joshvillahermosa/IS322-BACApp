fba2a77440660ae4363dec38069aa9da9f0095fe
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-11 14:27:57 -0500

    Removed confusing code written in the past

--- js/views/Area.js
+++ js/views/Area.js
@@ -5,16 +5,14 @@ var Area = Backbone.View.extend({
 		//this.getLocation();
 		this.canvasId = 'map';
 		this.area = '<h2>Area of BAC</h2><div id="'+this.canvasId+'"></div>';
+		google.maps.event.addDomListener(window, 'load', this);
+		window.lat = 0;
+		window.lng = 0;
 	},
 
 	render: function(){
 		this.$el.html(this.area);
-		this.getLocation();
-		//console.log(curLocation.lat+' '+curLocation.lng+' - Confirmed');
-		//this.createMap(this.lat, this.lng);
-
-
-		//google.maps.event.addDomListener(window, 'load', this.createMap);
+		this.createMap();
 		
 	},
 
@@ -30,43 +28,6 @@ var Area = Backbone.View.extend({
 		var map = new google.maps.Map(document.getElementById(this.canvasId), mapOptions);
 	},
 
-	getLocation: function(){
-		if(navigator.geolocation){
-			var location = navigator.geolocation.getCurrentPosition(this.setLatLng);
-			/*navigator.geolocation.getCurrentPosition(function(position){
-				var location = {"lat": 0, "lng":0};
-				location.lat = position.coords.latitude;
-				location.lng = position.coords.longitude;
-				return location;
-			});*/
-		}else{
-			alert('Either you have no internet connection or your browser is not supported');
-			console.log('Either you have no internet connection or your browser is not supported');
-			var location = null;
-		}
-		return location;
-	},
-
-	setLatLng: function(position){
-		var location = {lat: 0, lng: 0};
-		console.log('setLatLng() called');
-		location.lat = position.coords.latitude;
-		console.log('Lat recieved');
-		location.lng = position.coords.longitude;
-		console.log('Lng recieved');
-		console.log(location.lat+' '+location.lng);
-		var mapOptions = {
-			center: new google.maps.LatLng(40.7430473, -74.1777488),
-			//center: new google.maps.LatLng(lat, lng),
-			zoom: 15
-		}
-		var map = new google.maps.Map(document.getElementById(this.canvasId), mapOptions);
-		return location;
-	},
-
-	throwErr: function(){
-		alert('Cannot get coords');
-	}
 
 })
 
