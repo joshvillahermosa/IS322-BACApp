018fc982d6ae101aafca980c258482e9c99d400a
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-11 14:58:43 -0500

    Changed functions type renering

--- js/views/Area.js
+++ js/views/Area.js
@@ -3,6 +3,7 @@ var Area = Backbone.View.extend({
 
 	initialize: function(){
 		//this.getLocation();
+		this.getCurrentLocation();
 		this.canvasId = 'map';
 		this.area = '<h2>Area of BAC</h2><div id="'+this.canvasId+'"></div>';
 		google.maps.event.addDomListener(window, 'load', this);
@@ -13,7 +14,7 @@ var Area = Backbone.View.extend({
 	render: function(){
 		this.$el.html(this.area);
 		this.createMap();
-		this.getCurrentLocation();
+		this.getCurrentLocationMap();
 		console.log('Check location: '+window.lat+' : '+window.lng);
 		//Does not store local
 		
@@ -31,7 +32,7 @@ var Area = Backbone.View.extend({
 		var map = new google.maps.Map(document.getElementById(this.canvasId), mapOptions);
 	},
 
-	getCurrentLocation: function(){
+	getCurrentLocationMap: function(){
 		if (navigator.geolocation){
 		    navigator.geolocation.getCurrentPosition(function(position){
 		    	console.log('Getting current location....');
@@ -45,9 +46,27 @@ var Area = Backbone.View.extend({
 		}else{
 			alert('Please turn on GPS');
 		}
+	},
+
+	getCurrentLocation: function(){
+		if (navigator.geolocation){
+		    navigator.geolocation.getCurrentPosition(function(position){
+		    	console.log('Getting current location....');
+		    	var loc = {
+		    		lat: 0,
+		    		lng: 0
+		    	};
+		    	window.lat = position.coords.latitude;
+		    	window.lng = position.coords.longitude;
+		    	loc.lat = position.coords.latitude;
+		    	loc.lng = position.coords.longitude;
+		    	console.log('...Location found: '+window.lat+' : '+window.lng);
+		    	return loc;
+		    }.bind(this));
+		}else{
+			alert('Please turn on GPS');
+		}
 	}
-
-
-})
+});
 
 var area = new Area()
