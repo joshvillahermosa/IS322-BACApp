788f1ff3b72a2cd4e3d582d99683261123be5965
joshvillahermosa <joshvillahermosa@gmail.com>
2014-05-11 20:57:49 -0500

    Edited form

--- js/views/Setup.js
+++ js/views/Setup.js
@@ -2,17 +2,26 @@ var Setup =  Backbone.View.extend({
 	el: '#load',
 
 	events:{
+		"click #agreement": "createProfileForm", 
 		"click #create": "createProfile"
 		//"onchange": "createProfile"
 	},
 
 	initialize: function(){
+		this.intro = '<div class=""><h1><i>Pleasure begins with control</i></h1><h2>Blood Alcohol Content Diary (Temp Name: NJIT Wasted Meter) v0.8</h2></div><p>This will help calculate a rough estimate of your blood alcohol content while drinking. This app will give you real time updating BAC as each minute passes by. It will track you highest Blood Alcohol Content, time, and location when you are at a bar or enjoying a party. This app is in early beta and will not be completed for a while<b>PLEASE NOTE THIS IS NOT A SUBSTITUTE FOR A BREATHALYZER!</b> This does not calculate the most accurate BAC of your body, there are missing variables (such as food digested, personal metabolism rate, urination, etc) that prevents you giving the most accurate BAC. What we do is give you a estimate.</p><p><b>By clicking next, you agree that this app is not a substitute for a breathalyzer. You understand does not give you the most accurate BAC level and you understand that you must use your own judgment when performing actions while under the influence.</b></p><button id="agreement" class="btn btn-success">Yes, I agree. LETS DRINK!</button>';
+
 		this.form = '<div class="" id="form1"><h2>Set your self up!</h2><div class="form-group"><label for="name">Name</label><input id="name" name="name" type="text" class="form-control" placeholder="Name" required></div><div class="form-group"><label for="email">Email</label><input id="email" name="email" type="text" class="form-control" placeholder="Email" required></div><div class="from-group"><label for="birthday">Birthday</label><input id="birthday" type="date" name="birthday" class="form-control" required></div><div class="from-group"><label for="weight">Weight</label><input id="weight" type="number" min="50" max="500" name="weight" class="form-control" required></div><div class="radio-inline"><div class="row"><div class="col-xs-6"><label><input type="radio" name="sex" value="0.73">Male</label></div><div class="col-xs-6"><label><input type="radio" name="sex" value="0.66">Female</label></div></div></div><br><!--<button class="btn btn-success" id="next">Next <i class="fa fa-arrow-right"></i></button>--></div><div class="" id="form2"><h2>Pick your drinks</h2><label>Beer Choice 1</label><select class="form-control" id="beer1"></select><label>Beer Choice 2</label><select class="form-control" id="beer2"></select><label>Beer Choice 3</label><select class="form-control" id="beer3"></select><label>Wine Choice 1</label><select class="form-control" id="wine1"></select><label>Wine Choice 2</label><select class="form-control" id="wine2"></select><label>Wine Choice 3</label><select class="form-control" id="wine3"></select><label>Liqour Choice 1</label><select class="form-control" id="hl1"></select><label>Liqour Choice 2</label><select class="form-control" id="hl2"></select><label>Liqour Choice 3</label><select class="form-control" id="hl3"></select><br><!--<button class="btn btn-success" id="prev">Next <i class="fa fa-arrow-left"></i></button>--><button type="button" class="btn btn-success" id="create">Set</button></div>';
 
 		//this.render();
 	},
 
 	render: function(){
+		$('#navis').attr('class', 'invisible');
+		this.$el.html(this.intro);
+		
+	},
+
+	createProfileForm: function(){
 		this.newSave = new People();
 		this.$el.html(this.form);
 		this.loadBar();
